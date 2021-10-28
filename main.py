25d5a47d0a96a5a1a4a9c3813e22f29533ca71aa
Egor <777egorkalinin777@gmail.com>
2021-10-28 10:24:45 +0300

    HomeWork

--- main.py
+++ main.py
@@ -1,45 +1,50 @@
-"""Ex 1"""
+"""
+Калинин Егор 9 класс
+30.10.21
+Ex 1
+"""
+num = 2
+random_list = [1, 'Hi', True, ['My', 3], (1, num, 3, [4.3, 4.6], 5)]
+"""Ex2"""
 
+random_list.append('String')
 
-massiv = []  # random int and len massiv
-result_dict = {}
-m = 0
-r = None
-for i in massiv:
-    if i in result_dict:
-        result_dict[i] += 1
-    else:
-        result_dict[i] = 1
+"""Ex3"""
 
-for k, i in result_dict.items():
-    if m < i:
-        m = i
-        r = k
+random_list[0] = 10
 
-print(r)  # O(N) (Линейная сложность алгоритма (вроде))
+"""Ex4"""
 
-"""Ex 2"""
-result = []
-S = 'SSSJSSJSJJJSJSJSJSJJ'  # random str
-J = 'SSSJJSJSJJSJSJSJSSJJ'  # random str
-r1 = ''
+rl = [9, 8, 76]
+random_list.append(rl)
 
-for j in J:
-    r1 += j  # Да, резы. Но другое в голову не приходет. Я вообще сомневаюсь, что я правильно задачу понял АХАХАХАХАХАХ
-    if r1 in S:
-        result.append(r1)
-    else:
-        r1 = ''
-print(", ".join(result))
-"""Ex 3"""
+"""Ex5"""
 
+random_tuple = ('Hash', 'Memory')
+random_list[1] = random_tuple
 
-massiv = [8, 6, 6, 4, 3, 2, 2, 1]  # random int and len massiv
-c = 0
-result = []
-for i in massiv:
-    if i != c:
-        c = i
-        result.append(i)
+"""Ex6"""
 
-print(result)  # O(N) (Получается тоже линейная сложность (вроде))
+print(random_list[4])
+
+"""Ex 7"""
+
+del random_list[3]
+
+"""Ex 8"""
+
+list_input = input('list')
+element_input = input('element')
+
+"""Ex9 """
+
+name = input()
+print(f"Hello, {name}!")  # Python 3.7
+
+"""Ex 10"""
+
+answers = []
+for _ in range(3):
+    answers.append(input())
+
+print(int(max(answers)) - int(min(answers)))
