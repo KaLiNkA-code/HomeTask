b8e0e804c1452731d98aaadb3c8d5b475608907f
Egor <777egorkalinin777@gmail.com>
2021-11-04 15:15:43 +0300

    HomeWork

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 Ex 1 сумма трех чисел
 """
 
-
 IntInput = [int(input()) for i in range(3)]
 print(f"Сумма: {sum(IntInput)}")
+
+"""Ex 2"""
+
+value = int(input("Введите число"))
+print(f"предыдущие число: {value-1}\nследующее число: {value+1}")
