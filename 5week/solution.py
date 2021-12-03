2602fa6ab7118a90b3bb42bda5361ccb30004874
Egor <777egorkalinin777@gmail.com>
2021-12-03 10:43:26 +0300

    HomeWork start week 5 try 1

--- 5week/solution.py
+++ 5week/solution.py
@@ -1,5 +1,11 @@
 import pandas
+path1 = input('Введите путь до 1 файла: ')
+path2 = input('Введите путь до 2 файла: ')
 
 
-with open('links001.csv', 'r') as f:
+with open('links001.csv', 'r') as f:  # in the future 'path1'
+    pass
+
+
+with open('links001.csv', 'r') as ff:  # in the future 'path2'
     pass
