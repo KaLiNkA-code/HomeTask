2044b3959fd501bd305f90413806db4fb3f8c5f3
Egor <777egorkalinin777@gmail.com>
2021-12-19 10:44:42 +0300

    HomeWork start week 5 try 4

---
+++ 7week/main7.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+stud = pd.read_csv("StudentsPerformance.CSV")
+
+a = stud[stud.loc['writing score'] & stud.loc['writing score'] > 0]
+
+print((stud.loc[:6]).shape)  #
+print((stud.iloc[:7]).shape)  #
+print((stud.tail(7)).shape)  #
+print((stud.iloc[0:7]).shape)
+print((stud.loc[:7]).shape)
+print((stud.head(7)).shape)  #
