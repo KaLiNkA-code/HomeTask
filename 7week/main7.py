07f98dc6d0f5e09cb2e00647b0fe5b735c5fe4fe
Egor <777egorkalinin777@gmail.com>
2021-12-22 23:00:22 +0300

    HomeWork start week 5 try 4

--- 7week/main7.py
+++ 7week/main7.py
@@ -8,5 +8,5 @@ print((stud.loc[:6]).shape)  #
 print((stud.iloc[:7]).shape)  #
 print((stud.tail(7)).shape)  #
 print((stud.iloc[0:7]).shape)
-print((stud.loc[:7]).shape)
+print((stud.loc[:7]).shape)  # yhih
 print((stud.head(7)).shape)  #
